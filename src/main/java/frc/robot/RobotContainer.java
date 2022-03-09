WarriorRobots/rapidrecharge-season

commit 16c36670737f5c078caaa7c14b26c39fea4e9000

Fix Zero and stabilize
Telling the arm to stabilize would run into the 3d printed blocks so
changing the command after zeroing to the in position solves this
problem

src/main/java/frc/robot/RobotContainer.java
@@ -279,7 +279,7 @@ public class RobotContainer {
 
     IO.rightJoystick_1.whileHeld(m_ShooterButton);
     IO.rightJoystick_2.whileHeld(m_DriverIntakeSequence).whenReleased(m_ArmPosition0);
-    IO.rightJoystick_12.whenPressed(m_ArmZero.andThen(m_ArmStabilize));
+    IO.rightJoystick_12.whenPressed(m_ArmZero.andThen(m_ArmPositionIN)); // arm is commanded to the IN position instead of stazilize because commanding stabilize runs into the 3d printed blocks
   }
 
   /**

commit 533154b81c1f5fc7f9bf09c2cb9d04ff33be5094

Fix Joystick Right 12
Fix Illegal Argument exception caused by 16c36670

src/main/java/frc/robot/RobotContainer.java
@@ -281,7 +281,7 @@ public class RobotContainer {
     IO.leftJoystick_4.whileHeld(m_ShooterBoostButton);
     IO.rightJoystick_1.whileHeld(m_ShooterButton);
     IO.rightJoystick_2.whileHeld(m_DriverIntakeSequence).whenReleased(m_ArmPosition0);
-    IO.rightJoystick_12.whenPressed(m_ArmZero.andThen(m_ArmPositionIN)); // arm is commanded to the IN position instead of stazilize because commanding stabilize runs into the 3d printed blocks
+    IO.rightJoystick_12.whenPressed(m_ArmZero.andThen(new ArmPosition(m_ArmSubsytem, Vars.ARM_IN))); // arm is commanded to the IN position instead of stazilize because commanding stabilize runs into the 3d printed blocks
   }
 
   /**